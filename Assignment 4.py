72d68a7c93c9fad381ba5c5f9fc63b9aa5d7b91b
J-Murage <90998532+J-Murage@users.noreply.github.com>
2021-09-21 09:00:28 +0300

    Python Assignments

---
+++ Assignment 4.py
@@ -0,0 +1,6 @@
+width =float(input("Enter the width of the rectangle: "))
+length =float(input("Enter the length of the rectangle: "))
+area = width * length
+perimeter = (width * length) * 2
+print("Area of the rectangle is %.2f" %area)
+print("Perimeter of the rectangle is %.2f" %perimeter)
